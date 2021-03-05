ed2bc387b3fac3862dcb4d3871db9b82babc1820
Greg <greg@passivelogic.com>
2021-03-04 20:15:13 -0700

    Initial Commit

---
+++ SwiftuiChartProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftuiChartProj
+//
+//  Created by Greg Hughes on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
